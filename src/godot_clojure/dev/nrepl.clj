8a7d4ca1fbc0fc3deb5b61b40bc2601a7f6d9d09
3starblaze <3starblaze@protonmail.com>
2024-06-15 16:02:40 +0300

    Use CIDER nrepl handler

--- src/godot_clojure/dev/nrepl.clj
+++ src/godot_clojure/dev/nrepl.clj
@@ -1,6 +1,7 @@
 (ns godot-clojure.dev.nrepl
   "Namespace responsible for providing NREPL with access to Godot."
   (:require
+   [cider.nrepl :refer [cider-nrepl-handler]]
    [nrepl.server :refer [start-server]]))
 
 (def server (atom nil))
@@ -9,5 +10,5 @@
 
 (defn entry-fn [new-p-get-proc-address]
   (reset! p-get-proc-address new-p-get-proc-address)
-  (reset! server (start-server :port 4200))
+  (reset! server (start-server :port 4200 :handler cider-nrepl-handler))
   (println "NREPL server has been started at localhost:4200"))
